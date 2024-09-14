0990c15fe6824e4cf7b587e0be7e387a90bf10d7
Gabriel Carbonera Orlando <gabrielorlando@live.com>
2024-09-14 10:08:18 -0300

    Create calculadora.sh
    Create File - Script para execução da calculadora

---
+++ calculadora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script para abertura da calculadora do Gabriel
+
+echo "Bem vindo a essa maravilhosa calculadora."
+echo "Carregando informaçoes da calculadora." #textos para ficar bonitinho
+echo "Calculadora carregada e pronta para uso."
+
+python3 orlando_calculator.py
+
